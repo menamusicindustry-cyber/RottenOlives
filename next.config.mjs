1e351707a371b625b47093c111120006e14c4e4b
menamusicindustry-cyber <menamusicindustry@gmail.com>
2025-08-27 14:27:00 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,6 @@
-const nextConfig = { experimental:{serverActions:true}, images:{ remotePatterns:[{ protocol:'https', hostname:'**' }] } };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Remove experimental.serverActions (no longer needed in Next 14)
+  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
+};
 export default nextConfig;
